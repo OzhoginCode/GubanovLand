29be8ff3a24c3e58beeb146214c08f0cc88868c5
OzhoginCode <danozh.b@gmail.com>
2024-01-07 21:44:58 +0200

    add scroll to the top on clicking header logo

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ const menuButtonImg = document.querySelector('.header-menu-button-img');
 const menuButton = document.querySelector('.header-menu-button');
 const navButtons = document.querySelectorAll('.nav-ul a');
 const applyButton = document.querySelector('.right-button-header');
+const logoButton = document.querySelector('.div-logo');
 
 const openedMenuButtonImgSrc = './media/header-menu-button-opened.svg';
 const closedMenuButtonImgSrc = './media/header-menu-button.svg';
@@ -19,5 +20,6 @@ const toggleMenu = () => {
 };
 
 menuButton.addEventListener('click', toggleMenu);
-navButtons.forEach((btn) => btn.addEventListener('click', toggleMenu));
+navButtons.forEach((btn) => btn.addEventListener('click', () => (isOpen ? toggleMenu() : null)));
 applyButton.addEventListener('click', () => (isOpen ? toggleMenu() : null));
+logoButton.addEventListener('click', () => (isOpen ? toggleMenu() : null));
