f79e6e7ac4e12e5939d2485d7e05c46320f5cf4e
OzhoginCode <danozh.b@gmail.com>
2024-04-04 23:17:50 +0200

    linting

--- app/bin/index.js
+++ app/bin/index.js
@@ -3,5 +3,5 @@ import server from '../server.js';
 
 const port = 4000;
 server().listen(port, () => {
-  console.log(`Server was started on '${port}'`);
+  console.log(`Server was started on '${port}'`);  // eslint-disable-line
 });
