c93d76de4726e2f3d77af9dfb8c70392c6c84b4a
OzhoginCode <danozh.b@gmail.com>
2024-04-19 18:01:12 +0200

    update iti

--- statics/scripts/phoneInputs.js
+++ statics/scripts/phoneInputs.js
@@ -256,7 +256,7 @@ const i18n = {
 };
 
 const itiConfig = {
-  utilsScript: 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.1.4/build/js/utils.js',
+  utilsScript: 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.4/build/js/utils.js',
   showSelectedDialCode: true,
   nationalMode: false,
   autoPlaceholder: 'agressive',
