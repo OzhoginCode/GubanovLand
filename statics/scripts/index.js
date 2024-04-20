9da73992becf75fd556f97da417636345f3a4359
OzhoginCode <danozh.b@gmail.com>
2024-04-20 15:54:50 +0200

    seo optimization

--- statics/scripts/index.js
+++ statics/scripts/index.js
@@ -1,11 +1,9 @@
 import '../styles/main.css';
 import '../styles/intl-tel-input.css';
-// import './audio.js';
-// import './autoPlayVideos.js';
-// import './menu.js';
-// import './modal-block.js';
+import './audio.js';
+import './autoPlayVideos.js';
+import './menu.js';
+import './modal-block.js';
 // import './phoneInputs.js';
 // import './privacy-policy.js';
 // import './scrollAnimation.js';
-
-console.log('no js');
